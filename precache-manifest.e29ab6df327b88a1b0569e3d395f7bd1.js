5ee85c67ac76a907c6e931bfb2553937d04db594
Aaron <timbolaj@gmail.com>
2022-06-13 19:35:55 -0400

    Updates

---
+++ precache-manifest.e29ab6df327b88a1b0569e3d395f7bd1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc0ae262968c1acb765808755047c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b734ec9c94a60009e598",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d4278aaee2f27e6fff16",
+    "url": "/static/css/main.c7d38d48.chunk.css"
+  },
+  {
+    "revision": "b734ec9c94a60009e598",
+    "url": "/static/js/2.15927e06.chunk.js"
+  },
+  {
+    "revision": "e882c774dc533c5aecfe772b8bb50e93",
+    "url": "/static/js/2.15927e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4278aaee2f27e6fff16",
+    "url": "/static/js/main.274e61e8.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "a311db2b1542fefa4d1f37aad81f57d5",
+    "url": "/static/media/Pokedex-home.a311db2b.png"
+  },
+  {
+    "revision": "410920932207346d848b7872cd4850a4",
+    "url": "/static/media/mapbook.41092093.png"
+  },
+  {
+    "revision": "694740a422b6746b381ec7abbf3eae40",
+    "url": "/static/media/pathfinder.694740a4.png"
+  },
+  {
+    "revision": "80145ef255d8944aa4ee6d9a04aeb9be",
+    "url": "/static/media/profile_pic.80145ef2.jpg"
+  },
+  {
+    "revision": "0cf51c408ebfa2baee74133b4ef4bb27",
+    "url": "/static/media/scheduler.0cf51c40.png"
+  },
+  {
+    "revision": "deb9a5e7b99074538ffdcbb43d89cb5b",
+    "url": "/static/media/tweeter.deb9a5e7.png"
+  }
+]);
